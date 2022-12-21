040541595a132b7f8dace52c917a997e55a8d15a
UandersonSilva <uandersons753@gmail.com>
2022-12-21 11:57:55 -0300

    testbench package and top file added.

---
+++ no_pipeline/testbench/tb_pkg.sv
@@ -0,0 +1,38 @@
+package tb_pkg;
+    localparam WIDTH = 32;
+
+    typedef enum logic [5:0] {
+        _r_type = 6'b000000,
+        _j      = 6'b000010,
+        _beq    = 6'b000100,
+        _addiu  = 6'b001001,
+        _lw     = 6'b100011,
+        _sw     = 6'b101011,
+        _check  = 6'b111111                       
+    } opcode_t;
+
+    typedef enum logic [5:0] {
+        _add = 6'b100000,
+        _sub = 6'b100010,
+        _AND = 6'b100100,
+        _OR  = 6'b100101,
+        _slt = 6'b101010
+    } funct_t;
+
+    `include "./tb_classes/data_item.svh"
+    `include "./tb_classes/input_transaction.svh"
+    `include "./tb_classes/output_transaction.svh"
+    `include "./tb_classes/transaction_port.svh"
+
+    `include "./tb_classes/input_monitor.svh"
+    `include "./tb_classes/output_monitor.svh"
+
+    `include "./tb_classes/scoreboard.svh"
+
+    `include "./tb_classes/driver.svh"
+    `include "./tb_classes/env.svh"
+    
+    `include "./tb_classes/tests/base_test.svh"
+    `include "./tb_classes/tests/initial_test.svh"
+
+endpackage : tb_pkg
---
+++ no_pipeline/testbench/tb_top.sv
@@ -0,0 +1,56 @@
+`include "../device/mips_np.sv"
+`include "tb_pkg.sv"
+`include "mips_interface.sv"
+
+module tb_top;
+    import tb_pkg::*;
+
+    mips_interface mips_if();
+
+    mips_np mnp0(
+        .clock_in(mips_if.clock), 
+        .reset_in(mips_if.reset_in), 
+        .instr_clock_in(mips_if.clock), 
+        .instrWrite_in(mips_if.instrWrite_in), 
+        .instr_address_in(mips_if.instr_address_in), 
+        .read_data_address_in(mips_if.read_data_address_in),
+        .instr_in(mips_if.instr_in),
+        .read_instr_out(mips_if.read_instr_out), 
+        .read_data_out(mips_if.read_data_out)
+    );
+
+    initial
+    begin
+        string test_name;
+
+        base_test t0;
+        
+        $value$plusargs("TEST=%s", test_name);
+
+        case(test_name)
+            "INITIAL_TEST":
+            begin
+                initial_test selected;
+                selected = new();
+                t0 = selected;
+            end
+
+            default:
+            begin
+                initial_test selected;
+                selected = new();
+                t0 = selected;
+                test_name = $sformatf("DEFAULT(INITIAL_TEST)");
+            end
+        endcase
+
+        t0.env_r.mips_if = mips_if;
+
+        $display("%c[2;35m", 27, "[TB_TOP] Running %s", test_name);
+        $display("%c[0m", 27);
+        
+        t0.run();
+        #1;
+        $finish;
+    end
+endmodule : tb_top
